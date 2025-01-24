ec159000d7255dc73056a015715af604d5e96341
arronyeoman <arron.yeoman@yorksj.ac.uk>
2025-01-24 13:32:28 +0000

    added cyclical scaling to test uniforms, currently textures cyle from 1 to -1 scale

--- src/main/java/com/arronyeoman/graphics/Renderer.java
+++ src/main/java/com/arronyeoman/graphics/Renderer.java
@@ -19,6 +19,10 @@ public class Renderer {
 
     private int texture;
 
+    private double temp;
+    private double cycleScale;
+    private Vector4 scaleVector;
+
     public Renderer(Shader shader) {
         System.out.println("Renderer created");
         this.shader = shader;
@@ -26,6 +30,9 @@ public class Renderer {
 
     public void renderMesh(Mesh mesh, String textureName) {
         //System.out.println("Rendering Mesh");
+        temp += 0.02;
+        cycleScale = Math.sin(temp);
+        scaleVector = new Vector4((float)cycleScale, (float)cycleScale, 1.0f, 1.0f);
         //enable vertex array and indices array
         texture = Texture.loadTexture(textureName);
         GL30.glBindVertexArray(mesh.getVAO());
@@ -40,6 +47,7 @@ public class Renderer {
         //GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, mesh.getCBO());
 
         shader.bind();
+        shader.setUniform("scale", scaleVector);
 
         GL11.glDrawElements(GL11.GL_TRIANGLES, mesh.getIndices().length, GL11.GL_UNSIGNED_INT, 0);
         //System.out.println("indices length: " + mesh.getIndices().length);
